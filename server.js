c52704922dcaa3eb9dd6726755cf44a129404a70
alinctin <alinstrimbu@gmail.com>
2017-06-22 23:41:49 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var models  = require('./models');
+
+// define entity
+var Article = models.Article;
+
+// init express application
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// REST methods
+app.use(require("./routes/articles.js"));
+
+// include static files in the admin folder
+app.use('/sciencedb', express.static('sciencedb'));
+app.use(express.static('app'));
+
+// include swagger api docummentation
+var swaggerUi = require('swaggerize-ui'); // second change
+
+app.get('/swagger', function(req, res){
+    var api = require('./config/api.json');
+    api.host = undefined;
+    res.status(200).send(api);
+});
+
+app.use('/docs', swaggerUi({
+  docs: '/swagger'  
+}));
+
+app.listen(process.env.PORT);
